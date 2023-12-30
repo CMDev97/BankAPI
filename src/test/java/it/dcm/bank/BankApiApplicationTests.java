88ae9e16d7b2e2f92b172ddfcc075eb2206799ec
CMDev97 <cristopher.moccia@gmail.com>
2023-12-30 10:00:59 +0100

    first commit

---
+++ src/test/java/it/dcm/bank/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dcm.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
