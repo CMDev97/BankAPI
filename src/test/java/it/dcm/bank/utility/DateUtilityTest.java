291b824417b6b6fe5f6031521c3e4dbbf9fa5c18
CMDev97 <cristopher.moccia@gmail.com>
2023-12-31 10:19:57 +0100

    added valida range case test

--- src/test/java/it/dcm/bank/utility/DateUtilityTest.java
+++ src/test/java/it/dcm/bank/utility/DateUtilityTest.java
@@ -19,4 +19,36 @@ public class DateUtilityTest {
         assertEquals(expectedFormat, formattedDate);
     }
 
+    @Test
+    void validRange_ShouldReturnTrue_WhenFromIsBeforeTo() {
+        LocalDate from = LocalDate.of(2022, 1, 1);
+        LocalDate to = LocalDate.of(2022, 1, 2);
+
+        assertTrue(DateUtility.validRange(from, to));
+    }
+
+    @Test
+    void validRange_ShouldReturnFalse_WhenFromIsAfterTo() {
+        LocalDate from = LocalDate.of(2022, 1, 3);
+        LocalDate to = LocalDate.of(2022, 1, 2);
+
+        assertFalse(DateUtility.validRange(from, to));
+    }
+
+    @Test
+    void validRange_ShouldReturnFalse_WhenFromAndToAreEqual() {
+        LocalDate fromAndTo = LocalDate.of(2022, 1, 1);
+
+        assertFalse(DateUtility.validRange(fromAndTo, fromAndTo));
+    }
+
+    @Test
+    void validRange_ShouldReturnFalse_WhenEitherDateIsNull() {
+        LocalDate from = LocalDate.of(2022, 1, 1);
+
+        assertFalse(DateUtility.validRange(null, from));
+        assertFalse(DateUtility.validRange(from, null));
+        assertFalse(DateUtility.validRange(null, null));
+    }
+
 }
